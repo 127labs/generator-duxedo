dd3bcaf5da0bd895b93b007d9c171de1acbbd7fe
Faris Amali Alis <farisamalialis@ymail.com>
2016-04-16 06:06:06 +0800

    Change logInstructions to allow users to copy import codes

--- generators/module/index.js
+++ generators/module/index.js
@@ -1,5 +1,6 @@
 'use strict';
 var yeoman = require('yeoman-generator');
+var chalk = require('chalk');
 var path = require('path');
 
 module.exports = yeoman.Base.extend({
@@ -44,9 +45,13 @@ module.exports = yeoman.Base.extend({
   end: {
     logInstructions: function() {
       this.log('');
-      this.log('You have created your ' + this.name + ' module successfully \n');
-      this.log('Make sure to add this module to your rootReducer');
+      this.log('Great, now import the module generated into your root reducer in src/reducer.js:');
       this.log('');
+      this.log(chalk.green("import " + this.name + " from './modules/" + this.name + "'"));
+      this.log('');
+      this.log('and add the reducer as part of the combineReducers: ');
+      this.log('');
+      this.log(chalk.green("[" + this.name + ".constants.NAME]: " + this.name + ".reducer"));
     }
   }
 });
